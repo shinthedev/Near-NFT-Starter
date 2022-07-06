faea035e09a8a8c232819501f4d2d93c1daa209c
Shin <99096179+shinthedev@users.noreply.github.com>
2022-07-06 23:13:00 +0200

    Created the landing page

---
+++ src/lib/components/discoverCard.svelte
@@ -0,0 +1,25 @@
+<div class="mb-20 w-full px-4 md:w-1/2 md:px-6 lg:w-1/3">
+	<a class="group relative block rounded-md bg-black" href="">
+		<img
+			class="absolute inset-0 h-full w-full rounded-md object-cover opacity-75 transition-opacity  group-hover:opacity-50"
+			src="https://f8n-production-collection-assets.imgix.net/0x4a80De696B06584Ee1985C380cE176398118B5BC/1/nft.jpg?q=80&auto=format%2Ccompress&cs=srgb&max-w=1680&max-h=1680"
+			alt=""
+		/>
+		<div class="relative p-8">
+			<p class="text-sm font-medium uppercase tracking-widest text-pink-500">Developer</p>
+
+			<p class="text-2xl font-bold text-white">Barry Scott</p>
+
+			<div class="mt-64">
+				<div
+					class="translate-y-8 transform opacity-0 transition-all  group-hover:translate-y-0 group-hover:opacity-100"
+				>
+					<p class="text-sm text-white">
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis perferendis hic
+						asperiores quibusdam quidem voluptates doloremque reiciendis nostrum harum. Repudiandae?
+					</p>
+				</div>
+			</div>
+		</div>
+	</a>
+</div>
